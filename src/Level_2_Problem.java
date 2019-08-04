cac4944dbcddbb9425f060918af3bc0b7309477c
Daan Pol <daanpol@tutamail.com>
2019-08-04 10:05:53 +0200

    Added Level_2_Problem

---
+++ src/Level_2_Problem.java
@@ -0,0 +1,25 @@
+public class Level_2_Problem extends Arithmetic_Problem {
+
+    private static final int level=2;
+
+    public Level_2_Problem(){
+        firstNumber=generator.nextInt(10);
+        secondNumber=generator.nextInt(10);
+    }
+
+    public String displayProblem(){
+        return String.format("(Level 2) What is the sum of %d and %d: ", firstNumber, secondNumber);
+    }
+
+    public boolean checkAnswer(String response){
+        return(String.valueOf(firstNumber+secondNumber).equals(response));
+    }
+
+    public int getSolution(){
+        return firstNumber+secondNumber;
+    }
+
+    public int getLevel(){
+        return level;
+    }
+}
