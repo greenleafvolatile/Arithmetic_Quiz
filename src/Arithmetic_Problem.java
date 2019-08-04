dff0fa6479cbf862d7410365ebbc35d78ae9f35d
Daan Pol <daanpol@tutamail.com>
2019-08-04 09:59:07 +0200

    Added Arithmetic_Problem

---
+++ src/Arithmetic_Problem.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public abstract class Arithmetic_Problem {
+
+    public static final Random generator=new Random();
+    protected int firstNumber;
+    protected int secondNumber;
+
+
+    public abstract boolean checkAnswer(String response);
+
+    public abstract String displayProblem();
+
+    public abstract int getSolution();
+
+    public abstract int getLevel();
+}
+
