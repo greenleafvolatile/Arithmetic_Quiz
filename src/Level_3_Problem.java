9181a151812e11632c94241f3ecc01d6dbff6379
Daan Pol <daanpol@tutamail.com>
2019-08-04 10:06:13 +0200

    Added Level_3_Problem

---
+++ src/Level_3_Problem.java
@@ -0,0 +1,28 @@
+public class Level_3_Problem extends Arithmetic_Problem {
+
+
+
+    private static final int level=3;
+
+    public Level_3_Problem(){
+
+        firstNumber=generator.nextInt(10);
+        secondNumber=generator.nextInt(firstNumber+1);
+    }
+
+    public String displayProblem(){
+        return String.format("(Level 3) What is the difference between %d and %d?", firstNumber, secondNumber);
+    }
+
+    public boolean checkAnswer(String response){
+        return String.valueOf(firstNumber-secondNumber).equals(response);
+    }
+
+    public int getSolution(){
+        return firstNumber-secondNumber;
+    }
+
+    public int getLevel(){
+        return level;
+    }
+}
