78b889a609f3d79d0d4c21767374ebc0567ce53a
Daan Pol <daanpol@tutamail.com>
2019-08-13 10:11:31 +0200

    Added Arithmetic_Quiz_Demo

--- src/Arithmetic_Quiz_Demo.java
+++ src/Arithmetic_Quiz_Demo.java
@@ -4,7 +4,7 @@ public class Arithmetic_Quiz_Demo {
     public static void main(String[] args) {
 
         Arithmetic_Quiz_1_0_1 quiz=new Arithmetic_Quiz_1_0_1();
-        //quiz.startQuiz();
+        quiz.startQuiz();
     }
 
 }
